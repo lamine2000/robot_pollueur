23aec88549a577265a3934a88fd79925ce107fd6
lamine <lemzogueye.lg72@gmail.com>
2021-09-28 00:35:16 +0000

    test exo5

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,18 @@
 import monde.Monde;
+import pollueur_sauteur.PollueurSauteur;
+import pollueur_tout_droit.PollueurToutDroit;
+import robot.Robot;
 
 public class Main {
 
     public static void main(String[] args) {
-        Monde monde = new Monde();
-
-        monde.metPapierGras(5, 7);
-        monde.metPapierGras(9, 7);
-        monde.metPapierGras(9, 8);
-        monde.metPapierGras(0, 5);
-        monde.metPapierGras(1, 2);
-        monde.metPapierGras(3, 0);
-
+        Monde monde = new Monde(20, 20, false);
+        Robot r = new PollueurSauteur(3, 5, monde);
+        Robot r2 = new PollueurSauteur(-3, 6, monde);
+        Robot r1 = new PollueurToutDroit(6, monde);
+        //r1.parcourir();
+        //r2.parcourir();
+        //r.parcourir();
         System.out.println(monde);
-        System.out.println(monde.nbPapiersGras());
-
-        monde.prendPapierGras(5, 7);
-        System.out.println(monde);
-        System.out.println(monde.nbPapiersGras());
-        System.out.println(monde.estSale(0, 5));
     }
 }
