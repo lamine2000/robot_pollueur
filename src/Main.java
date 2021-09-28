55ac088087149c02c512eecb961a157f4ad5a4c2
lamine <lemzogueye.lg72@gmail.com>
2021-09-28 20:52:14 +0000

    text exo6 : Classe RobotNettoyeur

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@ import monde.Monde;
 import pollueur_sauteur.PollueurSauteur;
 import pollueur_tout_droit.PollueurToutDroit;
 import robot.Robot;
+import robot_nettoyeur.RobotNettoyeur;
 
 public class Main {
 
     public static void main(String[] args) {
-        Monde monde = new Monde(20, 20, false);
+        Monde monde = new Monde(20, 20, true);
         Robot r = new PollueurSauteur(3, 5, monde);
         Robot r2 = new PollueurSauteur(-3, 6, monde);
         Robot r1 = new PollueurToutDroit(6, monde);
-        r1.parcourir();
-        r2.parcourir();
-        r.parcourir();
+        //r1.parcourir();
+        //r2.parcourir();
+        //r.parcourir();
+        System.out.println(monde);
+
+        RobotNettoyeur r3 = new RobotNettoyeur(monde);
+        r3.parcourir();
         System.out.println(monde);
     }
 }
